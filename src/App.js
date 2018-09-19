965ae2b0a7e69fe1386af0db66f2ea4c788f0c95
Vele.Tosevski <vele.tosevski@moneris.com>
2018-09-19 17:50:08 -0400

    Added the svg to and started the structure of the home page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { css } from "emotion";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+import titleImg from "./media/image1.svg";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div
+        className={css`
+          display: flex;
+          flex-direction: column;
+          flex-wrap: no-wrap;
+          height: 100vh;
+        `}
+      >
+        <img src={titleImg} alt="Image of Vele speaking" />
       </div>
     );
   }
