f1fe4be9403f07ff665581420e6bc6b9b2442ee9
Vele.Tosevski <vele.tosevski@moneris.com>
2018-09-20 17:44:14 -0400

    added the button to try and icons.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { Helmet } from "react-helmet";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+//import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import { css } from "emotion";
+
+import "./util/icons.css";
 
 import titleImg from "./media/image1.svg";
 
@@ -14,14 +16,29 @@ class App extends Component {
           flex-direction: column;
           flex-wrap: no-wrap;
           height: 100vh;
+          width: 100vh;
+          justify-content: center;
         `}
       >
         <Helmet>
           <meta charSet="utf-8" />
           <title>Home</title>
-          <link rel="canonical" href="https://veletosevski.com" />
+          <link rel="canonical" href="https://www.veletosevski.com" />
         </Helmet>
-        <img src={titleImg} alt="Image of Vele speaking" />
+        <img src={titleImg} />
+        <div
+          className={css`
+            display: flex;
+            flex-direction: row;
+            flex-wrap: no-wrap;
+            justify-content: center;
+            position: absolute;
+          `}
+        >
+          <div class="block">
+            <i class="hovicon effect-1 sub-a">IO</i>
+          </div>
+        </div>
       </div>
     );
   }
