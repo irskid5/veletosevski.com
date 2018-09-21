51403e94a965c39a4864e326a1d0c34646e39047
Vele.Tosevski <vele.tosevski@moneris.com>
2018-09-21 12:47:58 -0400

    Fixed centering issues and added more buttons

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import "./util/icons.css";
 
 import titleImg from "./media/image1.svg";
 
+const code = "</>";
+
 class App extends Component {
   render() {
     return (
@@ -16,7 +18,7 @@ class App extends Component {
           flex-direction: column;
           flex-wrap: no-wrap;
           height: 100vh;
-          width: 100vh;
+          width: 100vw;
           justify-content: center;
         `}
       >
@@ -25,18 +27,25 @@ class App extends Component {
           <title>Home</title>
           <link rel="canonical" href="https://www.veletosevski.com" />
         </Helmet>
-        <img src={titleImg} />
+        <img src={titleImg} style={{ position: "absolute", zIndex: "0", height: "100vh", width: "100vw" }} alt="Background" />
         <div
           className={css`
             display: flex;
             flex-direction: row;
             flex-wrap: no-wrap;
             justify-content: center;
-            position: absolute;
+            position: relative;
+            z-index: 10;
           `}
         >
           <div class="block">
-            <i class="hovicon effect-1 sub-a">IO</i>
+            <i class="hovicon effect-1 sub-a"><img src="./media/icons/music-player.svg"/></i>
+          </div>
+          <div class="block">
+            <i class="hovicon effect-1 sub-a">{code}</i>
+          </div>
+          <div class="block">
+            <i class="hovicon effect-1 sub-a"><img src="./media/icons/macedonia.svg"/></i>
           </div>
         </div>
       </div>
