3684e22eb6ed08e141b751329e779089e45a48dc
Vele.Tosevski <vele.tosevski@moneris.com>
2018-09-22 00:19:45 -0400

    added the background slideshow and created special css files to make my life easier lmao

--- src/App.js
+++ src/App.js
@@ -2,48 +2,43 @@ import React, { Component } from "react";
 import { Helmet } from "react-helmet";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import { css } from "emotion";
+import Slideshow from "react-slidez";
 
 import { FaMusic } from "react-icons/fa";
 import { FaCode } from "react-icons/fa";
+
+import "./App.css";
 import "./util/icons.css";
 
 import titleImg from "./media/image1.svg";
-import maciIcon from "./media/icons/republic-of-macedonia.svg";
+import slide1 from "./media/slide1.jpg";
+import slide2 from "./media/slide2.jpg";
+import slide3 from "./media/slide3.jpg";
+import slide4 from "./media/slide4.jpg";
 import "./media/icons/macedonia/css/fontello.css";
 
 class App extends Component {
   render() {
     return (
-      <div
-        className={css`
-          display: flex;
-          flex-direction: column;
-          flex-wrap: no-wrap;
-          height: 100vh;
-          width: 100vw;
-          justify-content: center;
-        `}
-      >
+      <div className={"mainscreen fadeIn"}>
         <Helmet>
           <meta charSet="utf-8" />
           <title>Home</title>
           <link rel="canonical" href="https://www.veletosevski.com" />
         </Helmet>
-        <img
-          src={titleImg}
-          style={{ position: "absolute", zIndex: "0", height: "100vh", width: "100vw" }}
-          alt="Background"
+        <Slideshow
+          showIndex={false}
+          showArrows={false}
+          autoplay
+          enableKeyboard={false}
+          slideInterval={10000}
+          defaultIndex={0}
+          slides={[slide1, slide2, slide3, slide4]}
+          effect={"fade"}
+          height={"100%"}
+          width={"100%"}
         />
-        <div
-          className={css`
-            display: flex;
-            flex-direction: row;
-            flex-wrap: no-wrap;
-            justify-content: center;
-            position: relative;
-            z-index: 10;
-          `}
-        >
+        <div className={"centralButtons fadeIn"}>
           <div class="block">
             <i class="hovicon effect-1 sub-a">
               <FaMusic />
